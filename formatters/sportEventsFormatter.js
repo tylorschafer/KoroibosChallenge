65314d955920c61468d894e5fa685399807f040c
Tylor Schafer <tylorschafer@gmail.com>
2020-01-13 22:22:00 -0700

    Change knex environment in sportEventsFormatter

--- formatters/sportEventsFormatter.js
+++ formatters/sportEventsFormatter.js
@@ -1,4 +1,4 @@
-const environment = process.env.NODE_ENV || 'test'
+const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
