3c3134df89bf501feafcae308b5c67450e5d6a11
Tylor Schafer <tylorschafer@gmail.com>
2020-01-13 17:29:44 -0700

    Removed unused configuration in olympians.spec

--- tests/models/olympians.spec.js
+++ tests/models/olympians.spec.js
@@ -1,7 +1,3 @@
-var shell = require('shelljs')
-var request = require('supertest')
-var app = require('../../app')
-
 const environment = process.env.NODE_ENV || 'test'
 const configuration = require('../../knexfile')[environment]
 const database = require('knex')(configuration)
