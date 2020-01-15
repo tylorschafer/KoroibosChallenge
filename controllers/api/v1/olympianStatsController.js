e26cc9e31e5ce9e3260d4fe194cad911425f09fd
Tylor Schafer <tylorschafer@gmail.com>
2020-01-14 21:07:07 -0700

    Add OlympianstatsFormatter to format JSON response

--- controllers/api/v1/olympianStatsController.js
+++ controllers/api/v1/olympianStatsController.js
@@ -1,21 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-const AllStatsModel = require('../../../models/allStats.js')
-const AllStats = new AllStatsModel()
+const OlympianStatsFormatter = require('../../../formatters/olympianStatsFormatter')
+const OlympianStats = new OlympianStatsFormatter()
 
 router.get('/', async (request, response) => {
-  response.status(200).json({
-    olympian_stats: {
-      total_competing_olympians: await AllStats.olympianCount(),
-      average_weight: {
-        unit: 'kg',
-        male_olympians: await AllStats.averageWeight('M'),
-        female_olympians: await AllStats.averageWeight('F')
-      },
-      average_age: await AllStats.averageAge()
-    }
-  })
+  response.status(200).json(await OlympianStats.statsFormat())
 })
 
 module.exports = router;
