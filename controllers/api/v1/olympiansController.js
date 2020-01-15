ac57f55b991808a0e7a5bd399521c7fcd1920333
Tylor Schafer <tylorschafer@gmail.com>
2020-01-14 19:55:31 -0700

    Remove data base modules from olympiansController

--- controllers/api/v1/olympiansController.js
+++ controllers/api/v1/olympiansController.js
@@ -1,16 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-const environment = process.env.NODE_ENV || 'development'
-const configuration = require('../../../knexfile')[environment]
-const database = require('knex')(configuration)
-
 const OlympianModel = require('../../../models/olympian.js')
 const Olympian = new OlympianModel()
 
 router.get('/', async (request, response) => {
   if (Object.keys(request.query).includes('age')) {
-    if (request.query.age === 'oldest' || request.query.age == 'youngest') {
+    if (request.query.age === 'oldest' || request.query.age === 'youngest') {
       Olympian.ageSearch(request.query.age)
         .then((olympians) => {
           response.status(200).json(olympians)
