9e7a6d74d6fcd511c12aec79e11be4332e8b2ed0
Tylor Schafer <tylorschafer@gmail.com>
2020-01-14 20:55:42 -0700

    Remove uneeded database modules in olympianStatsController

--- controllers/api/v1/olympianStatsController.js
+++ controllers/api/v1/olympianStatsController.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-const environment = process.env.NODE_ENV || 'development'
-const configuration = require('../../../knexfile')[environment]
-const database = require('knex')(configuration)
-
 const AllStatsModel = require('../../../models/allStats.js')
 const AllStats = new AllStatsModel()
 
